a547620792fd980a48f3126d90570276994cc99d
LiXiaoping <xplisina@sina.com>
2018-12-07 09:50:30 +0800

    程序初始化

---
+++ grails-app/controllers/lims2018e/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package lims2018e
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
---
+++ grails-app/init/lims2018e/BootStrap.groovy
@@ -0,0 +1,9 @@
+package lims2018e
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
